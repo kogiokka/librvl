cc29cd8c155d6e2d05c52d50fdb47f78676a1019
luis <kogiokkafrms@gmail.com>
2022-10-08 07:58:30 +0000

    Refactor rvl_p and add some comments
    FossilOrigin-Name: 4c020ec712ece4feb2810daa84784590097b3c1219a0c9b9d28af3739d5dc0d6

--- include/detail/rvl_p.h
+++ include/detail/rvl_p.h
@@ -11,23 +11,26 @@ typedef uint8_t u8;
 typedef uint32_t u32;
 typedef const uint8_t rvlcbyte_t;
 
-typedef uint32_t RVLChunkCode_t;
-
-#define RVL_FILE_SIG_SIZE 12
-
-// .RVL FORMAT\0
-extern rvlbyte_t RVL_FILE_SIG[RVL_FILE_SIG_SIZE];
-
+/**
+ * Chunk code constants
+ *
+ * A chunk code is an unsigned integer that consists of human readable
+ * characters. CHUNK_CODE macro puts the code characters into the integer in
+ * little-endian order.
+ */
 #define SHIFT32(byte, numBits) ((u32)(byte) << numBits)
-
-// Little-endian
 #define CHUNK_CODE(b1, b2, b3, b4)                                            \
   (SHIFT32 (b4, 24) | SHIFT32 (b3, 16) | SHIFT32 (b2, 8) | b1)
 
+typedef uint32_t RVLChunkCode_t;
 #define RVLChunkCode_INFO CHUNK_CODE (73, 78, 70, 79)
 #define RVLChunkCode_DATA CHUNK_CODE (68, 65, 84, 65)
 #define RVLChunkCode_TEXT CHUNK_CODE (84, 69, 88, 84)
 #define RVLChunkCode_END CHUNK_CODE (69, 78, 68, 32)
+
+// RVL File Signature: .RVL FORMAT\0
+#define RVL_FILE_SIG_SIZE 12
+extern rvlbyte_t RVL_FILE_SIG[RVL_FILE_SIG_SIZE];
 
 struct RVL
 {
