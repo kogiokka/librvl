3fe02208a9218e4ed9df2f4157641e36eb9f0a55
luis <kogiokkafrms@gmail.com>
2022-10-08 07:40:21 +0000

    Replace the enum with defines for RVLChunkCode_t
    FossilOrigin-Name: f2742239dbe5fb39d44d92644ea69afc5d2966714c4f1a4b8b88e10a00a93ec9

--- include/detail/rvl_p.h
+++ include/detail/rvl_p.h
@@ -11,26 +11,23 @@ typedef uint8_t u8;
 typedef uint32_t u32;
 typedef const uint8_t rvlcbyte_t;
 
+typedef uint32_t RVLChunkCode_t;
 
 #define RVL_FILE_SIG_SIZE 12
 
 // .RVL FORMAT\0
 extern rvlbyte_t RVL_FILE_SIG[RVL_FILE_SIG_SIZE];
 
-
 #define SHIFT32(byte, numBits) ((u32)(byte) << numBits)
 
 // Little-endian
 #define CHUNK_CODE(b1, b2, b3, b4)                                            \
-  SHIFT32 (b4, 24) | SHIFT32 (b3, 16) | SHIFT32 (b2, 8) | b1
+  (SHIFT32 (b4, 24) | SHIFT32 (b3, 16) | SHIFT32 (b2, 8) | b1)
 
-typedef enum
-{
-  RVLChunkCode_INFO = CHUNK_CODE (73, 78, 70, 79),
-  RVLChunkCode_DATA = CHUNK_CODE (68, 65, 84, 65),
-  RVLChunkCode_TEXT = CHUNK_CODE (84, 69, 88, 84),
-  RVLChunkCode_END = CHUNK_CODE (69, 78, 68, 32),
-} RVLChunkCode_t;
+#define RVLChunkCode_INFO CHUNK_CODE (73, 78, 70, 79)
+#define RVLChunkCode_DATA CHUNK_CODE (68, 65, 84, 65)
+#define RVLChunkCode_TEXT CHUNK_CODE (84, 69, 88, 84)
+#define RVLChunkCode_END CHUNK_CODE (69, 78, 68, 32)
 
 struct RVL
 {
