b5ad1c7840a7ce582c08b273414a78d946f5cba6
luis <kogiokkafrms@gmail.com>
2022-10-23 04:46:47 +0000

    Do not build lz4cli
    FossilOrigin-Name: a55f3583f924fd601c8986fa86a7c40a7287d77b24d5ca1756b8b3abf4ab1536

--- cmake/fetch/FetchLZ4.cmake
+++ cmake/fetch/FetchLZ4.cmake
@@ -8,5 +8,8 @@ if(NOT lz4_POPULATED)
     set(FETCHCONTENT_UPDATES_DISCONNECTED ON)
     set(FETCHCONTENT_QUIET FALSE)
     FetchContent_MakeAvailable(lz4)
+
+    set(LZ4_BUILD_CLI OFF CACHE BOOL "Build lz4 program")
+    set(LZ4_BUILD_LEGACY_LZ4C OFF CACHE BOOL "Build lz4c program with legacy argument support")
     add_subdirectory("${lz4_SOURCE_DIR}/build/cmake/" "lz4")
 endif()
