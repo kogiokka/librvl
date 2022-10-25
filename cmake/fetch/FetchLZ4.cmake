396135d7163b97610811264c898b61564c1abc47
luis <kogiokkafrms@gmail.com>
2022-10-25 04:45:56 +0000

    Set SOURCE_SUBDIR for lz4
    FossilOrigin-Name: 17a993f7a80ec9669b5eae83e8b258f05098b0a861dc5d88beba11e8359d3cca

--- cmake/fetch/FetchLZ4.cmake
+++ cmake/fetch/FetchLZ4.cmake
@@ -2,14 +2,14 @@ FetchContent_Declare(
     lz4
     URL "${CMAKE_CURRENT_SOURCE_DIR}/external/lz4-1.9.4.zip"
     URL_HASH MD5=9c6b76f71921dd986468dcde7c095793
+    SOURCE_SUBDIR "build/cmake/" # Look for the CMakeLists.txt of lz4
 )
 
 if(NOT lz4_POPULATED)
     set(FETCHCONTENT_UPDATES_DISCONNECTED ON)
     set(FETCHCONTENT_QUIET FALSE)
-    FetchContent_MakeAvailable(lz4)
 
     set(LZ4_BUILD_CLI OFF CACHE BOOL "Build lz4 program")
     set(LZ4_BUILD_LEGACY_LZ4C OFF CACHE BOOL "Build lz4c program with legacy argument support")
-    add_subdirectory("${lz4_SOURCE_DIR}/build/cmake/" "lz4")
+    FetchContent_MakeAvailable(lz4)
 endif()
