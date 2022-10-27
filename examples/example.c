1f6ed760fda2d90627e7a6a6cb6a39c0f097c624
luis <kogiokkafrms@gmail.com>
2022-10-27 04:02:50 +0000

    Update example
    FossilOrigin-Name: 10bc5fe722e9ad06b5d4ef6daf2c3e1f8eafa234fcd77bf0d2af59d48aa5c837

--- examples/example.c
+++ examples/example.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,12 +15,12 @@ main ()
   RVL *rvl;
 
   rvl = rvl_create_writer ();
-  rvl_set_file (rvl, "volumetric_data.rvl");
+  rvl_set_file (rvl, "sphere.rvl");
   write_rvl (rvl);
   rvl_destroy (&rvl);
 
   rvl = rvl_create_reader ();
-  rvl_set_file (rvl, "volumetric_data.rvl");
+  rvl_set_file (rvl, "sphere.rvl");
   read_rvl (rvl);
   rvl_destroy (&rvl);
 
@@ -29,12 +30,13 @@ main ()
 void
 write_rvl (RVL *rvl)
 {
-  int nx = 20;
-  int ny = 10;
-  int nz = 20;
+  int nx = 9;
+  int ny = 16;
+  int nz = 25;
 
-  unsigned int size   = nx * ny * nz * sizeof (int);
-  int         *buffer = (int *)malloc (size);
+  unsigned int size      = nx * ny * nz * sizeof (int);
+  float       *buffer    = (float *)malloc (size);
+  float        center[3] = { nx * 0.5f, ny * 0.5f, nz * 0.5f };
 
   for (int i = 0; i < nz; i++)
     {
@@ -42,19 +44,25 @@ write_rvl (RVL *rvl)
         {
           for (int k = 0; k < nx; k++)
             {
-              buffer[k + j * nx + i * nx * ny] = i;
+              int   idx  = k + j * nx + i * nx * ny;
+              float dx   = (k - center[0]);
+              float dy   = (j - center[1]);
+              float dz   = (i - center[2]);
+              float dist = sqrt (dx * dx + dy * dy + dz * dz);
+
+              buffer[idx] = dist;
             }
         }
     }
 
   rvl_set_regular_grid (rvl, 0.1f, 0.2f, 0.1f);
-  rvl_set_volumetric_format (rvl, nx, ny, nz, RVL_PRIMITIVE_I32,
+  rvl_set_volumetric_format (rvl, nx, ny, nz, RVL_PRIMITIVE_F32,
                              RVL_ENDIAN_LITTLE);
   rvl_set_data_buffer (rvl, size, buffer);
 
   rvl_set_text (rvl, RVL_TAG_TITLE, "Sphere");
   rvl_set_text (rvl, RVL_TAG_DESCRIPTION,
-                "A volumetric model of an UV sphere.");
+                "Distances from the center of the volumetric data.");
   rvl_set_text (rvl, RVL_TAG_AUTHOR, "John Doe");
   rvl_set_text (rvl, RVL_TAG_COPYRIGHT,
                 "Copyright (c) 2022 by John Doe, All Rights Reserved.");
@@ -78,21 +86,24 @@ read_rvl (RVL *rvl)
   RVLenum primitive, endian;
   int     x, y, z;
   float   dx, dy, dz;
-  float   px, py, pz;
+  float   x0, y0, z0;
   rvl_get_volumetric_format (rvl, &x, &y, &z, &primitive, &endian);
   rvl_get_voxel_dims (rvl, &dx, &dy, &dz);
-  rvl_get_grid_position (rvl, &px, &py, &pz);
+  rvl_get_grid_position (rvl, &x0, &y0, &z0);
 
   char sep[81];
   memset (sep, '-', 80);
   sep[80] = '\0';
   fprintf (stdout, "%s\n", sep);
-  fprintf (stdout, "Width: %d, Length: %d, Height: %d\n", x, y, z);
-  fprintf (stdout, "Grid - type: %d, unit: %d\n", gridType, unit);
-  fprintf (stdout, "Data format: 0x%.4x\n", primitive);
-  fprintf (stdout, "Endian - %d\n", endian);
-  fprintf (stdout, "Voxel Dim - x: %.3f, y: %.3f, z: %.3f\n", dx, dy, dz);
-  fprintf (stdout, "Position - x: %.3f, y: %.3f, z: %.3f\n", px, py, pz);
+  fprintf (stdout, "Width (x): %d\n", x);
+  fprintf (stdout, "Length (y): %d\n", y);
+  fprintf (stdout, "Height (z): %d\n", z);
+  fprintf (stdout, "Grid type: 0x%.4X\n", gridType);
+  fprintf (stdout, "Data primitive: 0x%.4X\n", primitive);
+  fprintf (stdout, "Endianness: %d\n", endian);
+  fprintf (stdout, "Voxel dimensions: (%.3f, %.3f, %.3f)\n", dx, dy, dz);
+  fprintf (stdout, "Grid position of the origin: (%.3f, %.3f, %.3f)\n", x0, y0, z0);
+  fprintf (stdout, "Grid unit: 0x%.4X\n", unit);
   fprintf (stdout, "%s\n", sep);
 
   const void *buffer;
@@ -120,16 +131,16 @@ read_rvl (RVL *rvl)
 void
 print_data_buffer (int x, int y, int z, const void *buffer)
 {
-  const int *data = (int *)buffer;
+  const float *data = (float *)buffer;
   for (int k = 0; k < z; k++)
     {
       for (int j = 0; j < y; j++)
         {
           for (int i = 0; i < x - 1; i++)
             {
-              fprintf (stdout, "%d|", data[i + j * x + k * x * y]);
+              fprintf (stdout, "%6.3f|", data[i + j * x + k * x * y]);
             }
-          fprintf (stdout, "%d", data[x - 1 + j * x + k * x * y]);
+          fprintf (stdout, "%6.3f", data[x - 1 + j * x + k * x * y]);
           fprintf (stdout, "\n");
         }
       fprintf (stdout, "\n");
