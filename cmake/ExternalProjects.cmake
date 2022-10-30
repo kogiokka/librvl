7b4ed47272d1af6004a6d85bd699900ea6cb3056
luis <kogiokkafrms@gmail.com>
2022-10-30 13:18:57 +0000

    Set external projects visibility to hidden
    FossilOrigin-Name: 97cc2932796e5d392c7fbbbf758e79b44ba794afb5d90b88f904dffdff30e7e4

--- cmake/ExternalProjects.cmake
+++ cmake/ExternalProjects.cmake
@@ -6,6 +6,7 @@ set(LIBLZMA_SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/external/xz-5.2.7")
 set(BUILD_SHARED_LIBS OFF CACHE BOOL "Build liblzma as a shared library instead of static")
 add_subdirectory("${LIBLZMA_SOURCE_DIR}" EXCLUDE_FROM_ALL)
 set_target_properties(liblzma PROPERTIES POSITION_INDEPENDENT_CODE ON)
+target_compile_options(liblzma PRIVATE $<$<C_COMPILER_ID:GNU>:-fvisibility=hidden>)
 
 if (WIN32)
    target_include_directories(liblzma INTERFACE "$<BUILD_INTERFACE:${LIBLZMA_SOURCE_DIR}/src/liblzma/api>")
@@ -30,3 +31,4 @@ PRIVATE
    "${LZ4_SOURCE_DIR}/lz4hc.c"
    "${LZ4_SOURCE_DIR}/xxhash.c"
 )
+target_compile_options(lz4 PRIVATE $<$<C_COMPILER_ID:GNU>:-fvisibility=hidden>)
